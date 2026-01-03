4f5b25f6b334bf0883ab930e3eaef00eab52fa1c
Light-of-the-World <bestwickwade@gmail.com>
2026-01-02 20:05:01 -0600

    Metadata changes and fixed Continue Your Search

--- BasicModInfo.cs
+++ BasicModInfo.cs
@@ -20,11 +20,15 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "GuidingLight";
     public override string Author { get; init; } = "LightoftheWorld";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.1");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.2");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
-    public override List<string>? Incompatibilities { get; init; } = ["ReadJsonConfigExample"];
-    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-    public override string? Url { get; init; } = "https://github.com/sp-tarkov/server-mod-examples";
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = new()
+    {
+        {"com.wtt.commonlib", new SemanticVersioning.Range(">=2.0.0") },
+        {"com.lightoftheworld.questsextended", new SemanticVersioning.Range(">=4.0.2") }
+    };
+    public override string? Url { get; init; } = "https://github.com/Light-of-the-World/GuidingLight-SPT4.0-";
     public override bool? IsBundleMod { get; init; } = false;
     public override string? License { get; init; } = "MIT";
 }
